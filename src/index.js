1a1de8050597e53060f67cb39e913a27328e21f6
Natalia Kazakevich <Natallia_Kazakevich@mail.ru>
2018-10-05 23:37:24 +0300

    task 8

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
-}
+ 
+  let degreeOf = 0;
+  let zeros = 0;
+
+  let maxPrimeNumber =  (base) => {
+    let numberMultiplier = 2;
+
+    while (true) {
+      if (base % numberMultiplier === 0) {
+        base = base/numberMultiplier;
+        degreeOf++;
+      } else {
+        numberMultiplier++;
+        degreeOf = 0;
+      }
+
+      if (base === 1) return numberMultiplier;
+    }
+  };
+
+  let divider = maxPrimeNumber(base);
+
+  while (number > 0) {
+    number = Math.floor( number / divider);
+    zeros += number;
+  }
+  zeros = Math.floor(zeros/degreeOf);
+
+  return zeros;
+};
